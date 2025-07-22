f81bb889762cf935d4bdbfa2ac754e3a5f954bfd
Santosh mahapatra <164129549+777santosh@users.noreply.github.com>
2025-07-22 14:15:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed= new Typed(".text",{
-    strings:["AI/ML Developer","Vidieo Editior","Machine Learning Engineer "],
+    strings:["AI/ML Developer","Video Editor","Machine Learning Engineer ","Aspiring Datascientist"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
